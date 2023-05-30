13c8e586164e7d02c7bdf6b9ebf1dae6df2f02b1
UrielCh <uriel.chemouni@gmail.com>
2023-05-30 18:21:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Weather Icons icons for deno / Preact
+
+**License** [SIL OFL 1.1](http://scripts.sil.org/OFL)
+
+**Project** [https://erikflowers.github.io/weather-icons/](https://erikflowers.github.io/weather-icons/)
+
+[See available icons here](https://react-icons.github.io/react-icons/icons?name=wi)
+
+## import_map.json
+
+For a transparent usage:
+
+```json
+{
+  "imports": {
+    "preact": "https://esm.sh/preact@10.15.1",
+    "preact/": "https://esm.sh/preact@10.15.1/",
+    "react-icons/wi": "https://deno.land/x/react_icons_wi@1.0.0//mod.ts",
+  }
+}
+```
+
+## Direct import sample
+
+`import { WiAlien } from "https://deno.land/x/react_icons_wi@1.0.0/mod.ts"`
+
+## import_map import sample
+
+`import { WiAlien } from "react-icons/wi"`
+
---
+++ mod.ts
@@ -0,0 +1,254 @@
+// Copyright 2013-2022 the Weather Icons authors. All rights reserved. SIL OFL 1.1 (http://scripts.sil.org/OFL).
+/**
+ * # Weather Icons icons for deno / Preact
+ * 
+ * **License** [SIL OFL 1.1](http://scripts.sil.org/OFL)
+ * 
+ * **Project** [https://erikflowers.github.io/weather-icons/](https://erikflowers.github.io/weather-icons/)
+ * 
+ * [See available icons here](https://react-icons.github.io/react-icons/icons?name=wi)
+ * 
+ * ## import_map.json
+ * 
+ * For a transparent usage:
+ * 
+ * ```json
+ * {
+ *   "imports": {
+ *     "preact": "https://esm.sh/preact@10.15.1",
+ *     "preact/": "https://esm.sh/preact@10.15.1/",
+ *     "react-icons/wi": "https://deno.land/x/react_icons_wi@1.0.0//mod.ts",
+ *   }
+ * }
+ * ```
+ * 
+ * ## Direct import sample
+ * 
+ * `import { WiAlien } from "https://deno.land/x/react_icons_wi@1.0.0/mod.ts"`
+ * 
+ * ## import_map import sample
+ * 
+ * `import { WiAlien } from "react-icons/wi"`
+ * 
+ * @module
+ */
+
+export { WiAlien } from './ico/WiAlien.ts';
+export { WiBarometer } from './ico/WiBarometer.ts';
+export { WiCelsius } from './ico/WiCelsius.ts';
+export { WiCloudDown } from './ico/WiCloudDown.ts';
+export { WiCloudRefresh } from './ico/WiCloudRefresh.ts';
+export { WiCloudUp } from './ico/WiCloudUp.ts';
+export { WiCloud } from './ico/WiCloud.ts';
+export { WiCloudyGusts } from './ico/WiCloudyGusts.ts';
+export { WiCloudyWindy } from './ico/WiCloudyWindy.ts';
+export { WiCloudy } from './ico/WiCloudy.ts';
+export { WiDayCloudyGusts } from './ico/WiDayCloudyGusts.ts';
+export { WiDayCloudyHigh } from './ico/WiDayCloudyHigh.ts';
+export { WiDayCloudyWindy } from './ico/WiDayCloudyWindy.ts';
+export { WiDayCloudy } from './ico/WiDayCloudy.ts';
+export { WiDayFog } from './ico/WiDayFog.ts';
+export { WiDayHail } from './ico/WiDayHail.ts';
+export { WiDayHaze } from './ico/WiDayHaze.ts';
+export { WiDayLightWind } from './ico/WiDayLightWind.ts';
+export { WiDayLightning } from './ico/WiDayLightning.ts';
+export { WiDayRainMix } from './ico/WiDayRainMix.ts';
+export { WiDayRainWind } from './ico/WiDayRainWind.ts';
+export { WiDayRain } from './ico/WiDayRain.ts';
+export { WiDayShowers } from './ico/WiDayShowers.ts';
+export { WiDaySleetStorm } from './ico/WiDaySleetStorm.ts';
+export { WiDaySleet } from './ico/WiDaySleet.ts';
+export { WiDaySnowThunderstorm } from './ico/WiDaySnowThunderstorm.ts';
+export { WiDaySnowWind } from './ico/WiDaySnowWind.ts';
+export { WiDaySnow } from './ico/WiDaySnow.ts';
+export { WiDaySprinkle } from './ico/WiDaySprinkle.ts';
+export { WiDayStormShowers } from './ico/WiDayStormShowers.ts';
+export { WiDaySunnyOvercast } from './ico/WiDaySunnyOvercast.ts';
+export { WiDaySunny } from './ico/WiDaySunny.ts';
+export { WiDayThunderstorm } from './ico/WiDayThunderstorm.ts';
+export { WiDayWindy } from './ico/WiDayWindy.ts';
+export { WiDegrees } from './ico/WiDegrees.ts';
+export { WiDirectionDownLeft } from './ico/WiDirectionDownLeft.ts';
+export { WiDirectionDownRight } from './ico/WiDirectionDownRight.ts';
+export { WiDirectionDown } from './ico/WiDirectionDown.ts';
+export { WiDirectionLeft } from './ico/WiDirectionLeft.ts';
+export { WiDirectionRight } from './ico/WiDirectionRight.ts';
+export { WiDirectionUpLeft } from './ico/WiDirectionUpLeft.ts';
+export { WiDirectionUpRight } from './ico/WiDirectionUpRight.ts';
+export { WiDirectionUp } from './ico/WiDirectionUp.ts';
+export { WiDust } from './ico/WiDust.ts';
+export { WiEarthquake } from './ico/WiEarthquake.ts';
+export { WiFahrenheit } from './ico/WiFahrenheit.ts';
+export { WiFire } from './ico/WiFire.ts';
+export { WiFlood } from './ico/WiFlood.ts';
+export { WiFog } from './ico/WiFog.ts';
+export { WiGaleWarning } from './ico/WiGaleWarning.ts';
+export { WiHail } from './ico/WiHail.ts';
+export { WiHorizonAlt } from './ico/WiHorizonAlt.ts';
+export { WiHorizon } from './ico/WiHorizon.ts';
+export { WiHot } from './ico/WiHot.ts';
+export { WiHumidity } from './ico/WiHumidity.ts';
+export { WiHurricaneWarning } from './ico/WiHurricaneWarning.ts';
+export { WiHurricane } from './ico/WiHurricane.ts';
+export { WiLightning } from './ico/WiLightning.ts';
+export { WiLunarEclipse } from './ico/WiLunarEclipse.ts';
+export { WiMeteor } from './ico/WiMeteor.ts';
+export { WiMoonAltFirstQuarter } from './ico/WiMoonAltFirstQuarter.ts';
+export { WiMoonAltFull } from './ico/WiMoonAltFull.ts';
+export { WiMoonAltNew } from './ico/WiMoonAltNew.ts';
+export { WiMoonAltThirdQuarter } from './ico/WiMoonAltThirdQuarter.ts';
+export { WiMoonAltWaningCrescent1 } from './ico/WiMoonAltWaningCrescent1.ts';
+export { WiMoonAltWaningCrescent2 } from './ico/WiMoonAltWaningCrescent2.ts';
+export { WiMoonAltWaningCrescent3 } from './ico/WiMoonAltWaningCrescent3.ts';
+export { WiMoonAltWaningCrescent4 } from './ico/WiMoonAltWaningCrescent4.ts';
+export { WiMoonAltWaningCrescent5 } from './ico/WiMoonAltWaningCrescent5.ts';
+export { WiMoonAltWaningCrescent6 } from './ico/WiMoonAltWaningCrescent6.ts';
+export { WiMoonAltWaningGibbous1 } from './ico/WiMoonAltWaningGibbous1.ts';
+export { WiMoonAltWaningGibbous2 } from './ico/WiMoonAltWaningGibbous2.ts';
+export { WiMoonAltWaningGibbous3 } from './ico/WiMoonAltWaningGibbous3.ts';
+export { WiMoonAltWaningGibbous4 } from './ico/WiMoonAltWaningGibbous4.ts';
+export { WiMoonAltWaningGibbous5 } from './ico/WiMoonAltWaningGibbous5.ts';
+export { WiMoonAltWaningGibbous6 } from './ico/WiMoonAltWaningGibbous6.ts';
+export { WiMoonAltWaxingCrescent1 } from './ico/WiMoonAltWaxingCrescent1.ts';
+export { WiMoonAltWaxingCrescent2 } from './ico/WiMoonAltWaxingCrescent2.ts';
+export { WiMoonAltWaxingCrescent3 } from './ico/WiMoonAltWaxingCrescent3.ts';
+export { WiMoonAltWaxingCrescent4 } from './ico/WiMoonAltWaxingCrescent4.ts';
+export { WiMoonAltWaxingCrescent5 } from './ico/WiMoonAltWaxingCrescent5.ts';
+export { WiMoonAltWaxingCrescent6 } from './ico/WiMoonAltWaxingCrescent6.ts';
+export { WiMoonAltWaxingGibbous1 } from './ico/WiMoonAltWaxingGibbous1.ts';
+export { WiMoonAltWaxingGibbous2 } from './ico/WiMoonAltWaxingGibbous2.ts';
+export { WiMoonAltWaxingGibbous3 } from './ico/WiMoonAltWaxingGibbous3.ts';
+export { WiMoonAltWaxingGibbous4 } from './ico/WiMoonAltWaxingGibbous4.ts';
+export { WiMoonAltWaxingGibbous5 } from './ico/WiMoonAltWaxingGibbous5.ts';
+export { WiMoonAltWaxingGibbous6 } from './ico/WiMoonAltWaxingGibbous6.ts';
+export { WiMoonFirstQuarter } from './ico/WiMoonFirstQuarter.ts';
+export { WiMoonFull } from './ico/WiMoonFull.ts';
+export { WiMoonNew } from './ico/WiMoonNew.ts';
+export { WiMoonThirdQuarter } from './ico/WiMoonThirdQuarter.ts';
+export { WiMoonWaningCrescent1 } from './ico/WiMoonWaningCrescent1.ts';
+export { WiMoonWaningCrescent2 } from './ico/WiMoonWaningCrescent2.ts';
+export { WiMoonWaningCrescent3 } from './ico/WiMoonWaningCrescent3.ts';
+export { WiMoonWaningCrescent4 } from './ico/WiMoonWaningCrescent4.ts';
+export { WiMoonWaningCrescent5 } from './ico/WiMoonWaningCrescent5.ts';
+export { WiMoonWaningCrescent6 } from './ico/WiMoonWaningCrescent6.ts';
+export { WiMoonWaningGibbous1 } from './ico/WiMoonWaningGibbous1.ts';
+export { WiMoonWaningGibbous2 } from './ico/WiMoonWaningGibbous2.ts';
+export { WiMoonWaningGibbous3 } from './ico/WiMoonWaningGibbous3.ts';
+export { WiMoonWaningGibbous4 } from './ico/WiMoonWaningGibbous4.ts';
+export { WiMoonWaningGibbous5 } from './ico/WiMoonWaningGibbous5.ts';
+export { WiMoonWaningGibbous6 } from './ico/WiMoonWaningGibbous6.ts';
+export { WiMoonWaxing6 } from './ico/WiMoonWaxing6.ts';
+export { WiMoonWaxingCrescent1 } from './ico/WiMoonWaxingCrescent1.ts';
+export { WiMoonWaxingCrescent2 } from './ico/WiMoonWaxingCrescent2.ts';
+export { WiMoonWaxingCrescent3 } from './ico/WiMoonWaxingCrescent3.ts';
+export { WiMoonWaxingCrescent4 } from './ico/WiMoonWaxingCrescent4.ts';
+export { WiMoonWaxingCrescent5 } from './ico/WiMoonWaxingCrescent5.ts';
+export { WiMoonWaxingGibbous1 } from './ico/WiMoonWaxingGibbous1.ts';
+export { WiMoonWaxingGibbous2 } from './ico/WiMoonWaxingGibbous2.ts';
+export { WiMoonWaxingGibbous3 } from './ico/WiMoonWaxingGibbous3.ts';
+export { WiMoonWaxingGibbous4 } from './ico/WiMoonWaxingGibbous4.ts';
+export { WiMoonWaxingGibbous5 } from './ico/WiMoonWaxingGibbous5.ts';
+export { WiMoonWaxingGibbous6 } from './ico/WiMoonWaxingGibbous6.ts';
+export { WiMoonrise } from './ico/WiMoonrise.ts';
+export { WiMoonset } from './ico/WiMoonset.ts';
+export { WiNa } from './ico/WiNa.ts';
+export { WiNightAltCloudyGusts } from './ico/WiNightAltCloudyGusts.ts';
+export { WiNightAltCloudyHigh } from './ico/WiNightAltCloudyHigh.ts';
+export { WiNightAltCloudyWindy } from './ico/WiNightAltCloudyWindy.ts';
+export { WiNightAltCloudy } from './ico/WiNightAltCloudy.ts';
+export { WiNightAltHail } from './ico/WiNightAltHail.ts';
+export { WiNightAltLightning } from './ico/WiNightAltLightning.ts';
+export { WiNightAltPartlyCloudy } from './ico/WiNightAltPartlyCloudy.ts';
+export { WiNightAltRainMix } from './ico/WiNightAltRainMix.ts';
+export { WiNightAltRainWind } from './ico/WiNightAltRainWind.ts';
+export { WiNightAltRain } from './ico/WiNightAltRain.ts';
+export { WiNightAltShowers } from './ico/WiNightAltShowers.ts';
+export { WiNightAltSleetStorm } from './ico/WiNightAltSleetStorm.ts';
+export { WiNightAltSleet } from './ico/WiNightAltSleet.ts';
+export { WiNightAltSnowThunderstorm } from './ico/WiNightAltSnowThunderstorm.ts';
+export { WiNightAltSnowWind } from './ico/WiNightAltSnowWind.ts';
+export { WiNightAltSnow } from './ico/WiNightAltSnow.ts';
+export { WiNightAltSprinkle } from './ico/WiNightAltSprinkle.ts';
+export { WiNightAltStormShowers } from './ico/WiNightAltStormShowers.ts';
+export { WiNightAltThunderstorm } from './ico/WiNightAltThunderstorm.ts';
+export { WiNightClear } from './ico/WiNightClear.ts';
+export { WiNightCloudyGusts } from './ico/WiNightCloudyGusts.ts';
+export { WiNightCloudyHigh } from './ico/WiNightCloudyHigh.ts';
+export { WiNightCloudyWindy } from './ico/WiNightCloudyWindy.ts';
+export { WiNightCloudy } from './ico/WiNightCloudy.ts';
+export { WiNightFog } from './ico/WiNightFog.ts';
+export { WiNightHail } from './ico/WiNightHail.ts';
+export { WiNightLightning } from './ico/WiNightLightning.ts';
+export { WiNightPartlyCloudy } from './ico/WiNightPartlyCloudy.ts';
+export { WiNightRainMix } from './ico/WiNightRainMix.ts';
+export { WiNightRainWind } from './ico/WiNightRainWind.ts';
+export { WiNightRain } from './ico/WiNightRain.ts';
+export { WiNightShowers } from './ico/WiNightShowers.ts';
+export { WiNightSleetStorm } from './ico/WiNightSleetStorm.ts';
+export { WiNightSleet } from './ico/WiNightSleet.ts';
+export { WiNightSnowThunderstorm } from './ico/WiNightSnowThunderstorm.ts';
+export { WiNightSnowWind } from './ico/WiNightSnowWind.ts';
+export { WiNightSnow } from './ico/WiNightSnow.ts';
+export { WiNightSprinkle } from './ico/WiNightSprinkle.ts';
+export { WiNightStormShowers } from './ico/WiNightStormShowers.ts';
+export { WiNightThunderstorm } from './ico/WiNightThunderstorm.ts';
+export { WiRainMix } from './ico/WiRainMix.ts';
+export { WiRainWind } from './ico/WiRainWind.ts';
+export { WiRain } from './ico/WiRain.ts';
+export { WiRaindrop } from './ico/WiRaindrop.ts';
+export { WiRaindrops } from './ico/WiRaindrops.ts';
+export { WiRefreshAlt } from './ico/WiRefreshAlt.ts';
+export { WiRefresh } from './ico/WiRefresh.ts';
+export { WiSandstorm } from './ico/WiSandstorm.ts';
+export { WiShowers } from './ico/WiShowers.ts';
+export { WiSleet } from './ico/WiSleet.ts';
+export { WiSmallCraftAdvisory } from './ico/WiSmallCraftAdvisory.ts';
+export { WiSmog } from './ico/WiSmog.ts';
+export { WiSmoke } from './ico/WiSmoke.ts';
+export { WiSnowWind } from './ico/WiSnowWind.ts';
+export { WiSnow } from './ico/WiSnow.ts';
+export { WiSnowflakeCold } from './ico/WiSnowflakeCold.ts';
+export { WiSolarEclipse } from './ico/WiSolarEclipse.ts';
+export { WiSprinkle } from './ico/WiSprinkle.ts';
+export { WiStars } from './ico/WiStars.ts';
+export { WiStormShowers } from './ico/WiStormShowers.ts';
+export { WiStormWarning } from './ico/WiStormWarning.ts';
+export { WiStrongWind } from './ico/WiStrongWind.ts';
+export { WiSunrise } from './ico/WiSunrise.ts';
+export { WiSunset } from './ico/WiSunset.ts';
+export { WiThermometerExterior } from './ico/WiThermometerExterior.ts';
+export { WiThermometerInternal } from './ico/WiThermometerInternal.ts';
+export { WiThermometer } from './ico/WiThermometer.ts';
+export { WiThunderstorm } from './ico/WiThunderstorm.ts';
+export { WiTime1 } from './ico/WiTime1.ts';
+export { WiTime10 } from './ico/WiTime10.ts';
+export { WiTime11 } from './ico/WiTime11.ts';
+export { WiTime12 } from './ico/WiTime12.ts';
+export { WiTime2 } from './ico/WiTime2.ts';
+export { WiTime3 } from './ico/WiTime3.ts';
+export { WiTime4 } from './ico/WiTime4.ts';
+export { WiTime5 } from './ico/WiTime5.ts';
+export { WiTime6 } from './ico/WiTime6.ts';
+export { WiTime7 } from './ico/WiTime7.ts';
+export { WiTime8 } from './ico/WiTime8.ts';
+export { WiTime9 } from './ico/WiTime9.ts';
+export { WiTornado } from './ico/WiTornado.ts';
+export { WiTrain } from './ico/WiTrain.ts';
+export { WiTsunami } from './ico/WiTsunami.ts';
+export { WiUmbrella } from './ico/WiUmbrella.ts';
+export { WiVolcano } from './ico/WiVolcano.ts';
+export { WiWindBeaufort0 } from './ico/WiWindBeaufort0.ts';
+export { WiWindBeaufort1 } from './ico/WiWindBeaufort1.ts';
+export { WiWindBeaufort10 } from './ico/WiWindBeaufort10.ts';
+export { WiWindBeaufort11 } from './ico/WiWindBeaufort11.ts';
+export { WiWindBeaufort12 } from './ico/WiWindBeaufort12.ts';
+export { WiWindBeaufort2 } from './ico/WiWindBeaufort2.ts';
+export { WiWindBeaufort3 } from './ico/WiWindBeaufort3.ts';
+export { WiWindBeaufort4 } from './ico/WiWindBeaufort4.ts';
+export { WiWindBeaufort5 } from './ico/WiWindBeaufort5.ts';
+export { WiWindBeaufort6 } from './ico/WiWindBeaufort6.ts';
+export { WiWindBeaufort7 } from './ico/WiWindBeaufort7.ts';
+export { WiWindBeaufort8 } from './ico/WiWindBeaufort8.ts';
+export { WiWindBeaufort9 } from './ico/WiWindBeaufort9.ts';
+export { WiWindDeg } from './ico/WiWindDeg.ts';
+export { WiWindy } from './ico/WiWindy.ts';
