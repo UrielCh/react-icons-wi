9db20c52d633b542374e04510ff54138484f4eca
urielch <uriel.chemouni@gmail.com>
2023-05-31 17:30:34 +0300

    add deps.ts, use latest core module from jsdelivr

--- mod.ts
+++ mod.ts
@@ -17,7 +17,7 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/wi":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-wi@1.0.2/mod.ts",
+ *     "react-icons/wi":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-wi@1.0.3/mod.ts",
  *     "react-icons/wi/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-wi/ico/",
  *   }
  * }
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { WiAlien } from "https://deno.land/x/react_icons_wi@1.0.2/mod.ts"
+ * import { WiAlien } from "https://deno.land/x/react_icons_wi@1.0.3/mod.ts"
  * ```
  * 
  * ## import_map import sample
