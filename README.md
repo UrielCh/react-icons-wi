09364d371b538bb96e5f3b9874029e5da0d05d05
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:45 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/wi)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/wi
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/wi
 bunx jsr add @preact-icons/wi
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Weather Icons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/wi)](https://jsr.io/@preact-icons/wi)
-
-**License** [SIL OFL 1.1](http://scripts.sil.org/OFL)
-
-**Project** [https://erikflowers.github.io/weather-icons/](https://erikflowers.github.io/weather-icons/)
-
-[See available icons here](https://react-icons.deno.dev/wi)
-
-## install packahe
-
-```bash
-deno add @preact-icons/wi
-dnpx jsr add @preact-icons/wi
-pnpm dlx jsr add @preact-icons/wi
-bunx jsr add @preact-icons/wi
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import WiAlien from "react-icons/wi/WiAlien.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/wi)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/wi
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/wi
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Weather Icons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/wi)](https://jsr.io/@preact-icons/wi)
- * 
- * **License** [SIL OFL 1.1](http://scripts.sil.org/OFL)
- * 
- * **Project** [https://erikflowers.github.io/weather-icons/](https://erikflowers.github.io/weather-icons/)
- * 
- * [See available icons here](https://react-icons.deno.dev/wi)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/wi
- * dnpx jsr add @preact-icons/wi
- * pnpm dlx jsr add @preact-icons/wi
- * bunx jsr add @preact-icons/wi
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import WiAlien from "react-icons/wi/WiAlien.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
